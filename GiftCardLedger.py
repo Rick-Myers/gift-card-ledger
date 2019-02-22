869d36856fb4a17fb7a39df8bdadd40ede3c6bcc
Rick Myers <1986092+Rick-Myers@users.noreply.github.com>
2019-02-21 22:41:30 -0500

    Top header label added.

--- GiftCardLedger.py
+++ GiftCardLedger.py
@@ -10,7 +10,21 @@ class GiftCardLedger(tk.Tk):
         self.title("Gift Card Ledger")
         self.geometry("400x600")
 
+        # Screen label that appears at the top. It displays what screen is currently active.
+        self.top_label_var = tk.StringVar(self)
+        self.top_label = tk.Label(self, textvar=self.top_label_var, fg="black", bg="white", font=('Terminal', 28))
+        self.top_label.pack(side=tk.TOP, fill=tk.X)
+        # todo set the label when the screen is selected?
+        self.top_label_var.set("Gift Card Ledger")
+
 
 if __name__ == "__main__":
     gift_card_tracker = GiftCardLedger()
 gift_card_tracker.mainloop()
+
+'''
+used to print a list of font families available on the system.
+rom tkinter import Tk, font
+root = Tk()
+print(font.families())
+'''
