e2f5d597a7e8c6be75c22971073400dee55a29d3
Rick Myers <1986092+Rick-Myers@users.noreply.github.com>
2019-03-12 21:23:32 -0400

    Adding entry point for ease of use.

---
+++ __main__.py
@@ -0,0 +1,11 @@
+__author__ = "Rick Myers"
+
+import os
+
+from ledger import GiftCardLedger
+
+
+if not os.path.isfile('gift_cards.db'):
+    GiftCardLedger.initialize_db()
+gift_card_ledger = GiftCardLedger()
+gift_card_ledger.mainloop()
